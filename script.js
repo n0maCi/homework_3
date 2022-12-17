91131cd251f48fa7eb9abecae698ecc0f3ffcc3a
n0maCi <n0maci1337@gmail.com>
2022-12-17 19:06:55 +0300

    С1

---
+++ script.js
@@ -0,0 +1,55 @@
+const student = "Алтухов Максим Иванович"; 
+
+function cutString(str, n) {
+    return str.split(" ").splice(0, n).join(" ");
+};
+
+function getAgeOfUser(birthdate) {
+    return ((new Date().getTime() - new Date(birthdate)) / (24 * 3600 * 365.25 * 1000)) | 0;
+}; 
+
+function getResult(dataFromJson) {
+    let names = [];
+    let countOfMale = 0;
+    let countOfFemale = 0;
+    let sex;
+    for(let i = 0; i < dataFromJson.users.length; i++) {
+        let letter = []; 
+        let b = 0;
+        if (dataFromJson.users[i].sex == 'male') {
+            countOfMale++;
+            sex = 'М';
+        } else {
+            countOfFemale++;
+            sex = 'Ж';
+        };
+        for (let n = 1; n < dataFromJson.users[i].name.length; n++) {
+            if (dataFromJson.users[i].name[n] == dataFromJson.users[i].name[n].toUpperCase()) {
+                letter[b++] = dataFromJson.users[i].name[n];
+            };
+        };
+        names[i] = '<br>' + cutString(dataFromJson.users[i].name, 1) + ' ' + letter[1] + '.' + letter[3] + '.' + ',' + ' ' + sex  + ', ' + 'Возраст: ' + getAgeOfUser(dataFromJson.users[i].birthdate);
+    };
+    return `
+    <br>
+    Всего гостей: ${countOfFemale + countOfMale} <br>
+    Мужчин: ${countOfMale} <br>
+    Женщин: ${countOfFemale} <br>
+    Список гостей: 
+    ${names}`
+};
+
+function getDataFromJson() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'data.json');
+    request.onloadend = function() {
+        dataFromJson = JSON.parse(request.responseText);
+        getResult(dataFromJson);
+        document.getElementById("result").innerHTML = getResult(dataFromJson);
+    }
+    request.send();
+};
+
+getDataFromJson();
+
+document.getElementById("student").innerHTML = student;
